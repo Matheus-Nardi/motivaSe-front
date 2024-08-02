529afa2c6ce724f89a7a6bef64b27f2fa2508eae
Matheus <matheuznardi@gmail.com>
2024-08-02 12:42:15 -0300

    demo

---
+++ app.js
@@ -0,0 +1,28 @@
+const url = "http://localhost:8080/motiva-se/frases";
+
+async function consumirApi() {
+  try {
+    const response = await fetch(`${url}/${2}`);
+    if (!response.ok) {
+      throw new Error("Erro na rede ao buscar as frases");
+    }
+
+    const frase = await response.json();
+    exibirFrase(frase);
+  } catch (error) {
+    console.error("Erro ao buscar frases:", error);
+  }
+}
+
+function exibirFrase(frase) {
+  const textoFrase = document.getElementById("texto-frase");
+  const autorFrase = document.getElementById("autor-frase");
+
+  textoFrase.textContent = `"${frase.texto}"`;
+  autorFrase.textContent = frase.autor;
+
+  console.log(textoFrase);
+  console.log(autorFrase);
+}
+
+document.addEventListener("DOMContentLoaded", consumirApi);
